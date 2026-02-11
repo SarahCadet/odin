bbe007a822af4301448abe2ca807fa7755ec5c62
Sarah Cadet <scadet@bu.edu>
2026-02-11 13:38:44 -0500

    finished js exercises 1-5; 5 was trivial so didn't fully go through

---
+++ javascript-exercises-main/foundations/03_numberChecker/numberChecker.js
@@ -0,0 +1,10 @@
+function numberChecker(number) {
+  if (number % 10 == 0) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+// Do not edit below this line
+module.exports = numberChecker;
